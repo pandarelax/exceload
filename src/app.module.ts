89b4ff8da972dd8be0f0f84234a05efdec84db67
Efehan Turhan <94855256+pandarelax@users.noreply.github.com>
2024-01-08 16:48:34 +0300

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'sa',
+      password: 'root',
+      database: 'test',
+      models: [],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
