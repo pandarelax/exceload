8f7d4ccb9f81829d49d5712b34dfe514588bccf5
Efehan Turhan <94855256+pandarelax@users.noreply.github.com>
2024-01-09 05:21:31 +0300

    Update create-field migration schema

--- api/migrations/20240108235927-create-field.js
+++ api/migrations/20240108235927-create-field.js
@@ -19,6 +19,8 @@ module.exports = {
           model: 'grids',
           key: 'id',
         },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
       },
       isPrimary: {
         type: Sequelize.BOOLEAN,
